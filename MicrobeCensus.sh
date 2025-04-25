c36fbe67d65fa40171fd4a94c09a06355ebbad74
AlvaroRodriguezDelRio <40565547+AlvaroRodriguezDelRio@users.noreply.github.com>
2025-04-25 11:53:56 +0200

    Update MicrobeCensus.sh

--- MicrobeCensus.sh
+++ MicrobeCensus.sh
@@ -10,9 +10,9 @@
 
 export PATH=/scratch/alvarordr/soft/miniforge-pypy3/bin:/scratch/alvarordr/soft/miniforge/bin:/scratch/alvarordr/soft/miniforge-pypy3/bin:/scratch/alvarordr/soft/miniforge-pypy3/condabin:/usr/local/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/home/alvarordr/.dotnet/tools:/scratch/alvarordr/soft/mocat2/stable/2.1.3/src:/home/alvarordr/.local/bin:/home/alvarordr/bin:/scratch/alvarordr/soft/mocat2/stable/2.1.3/src
 
-file1=$(cat  paths_samples.txt | awk -v line=$SLURM_ARRAY_TASK_ID '{if (NR == line) print $0}')
-name=$(basename $file1 | sed 's/_1.fastq//g')
-file2=$(echo $file1 | sed 's/_1.fastq/_2.fastq/g')
+###Paths to files
+file1=$(cat paths_clean_1.txt | awk -v line=$SLURM_ARRAY_TASK_ID '{if (NR == line) print $0}')
+name=$(basename $file1 | sed 's/_kneaddata_paired_1.fastq//g')
+file2=$(echo $file1 | sed 's/paired_1/paired_2/g')
 
-
-python /scratch/alvarordr/field_experiment_metagenomes/analysis/MicrobeCensus/scripts/microbecensus.py $file1 $file2 > out/$name.tab
+python /scratch/alvarordr/field_experiment_metagenomes/analysis/MicrobeCensus/scripts/microbecensus.py $file1 $file2 > out_MicrobeCensus/$name.tab
