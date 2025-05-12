ef6c365dda5374ca3367b0f4b8176d2d9046432c
AlvaroRodriguezDelRio <40565547+AlvaroRodriguezDelRio@users.noreply.github.com>
2025-05-12 12:36:48 +0200

    Update funprofiler.sh

--- funprofiler.sh
+++ funprofiler.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --array=1-N_SAMPLES
-#SBATCH --output logs/%A_%a.out
-#SBATCH --error logs/%A_%a.error
+#SBATCH --output logs/funprofiler_%a.out
+#SBATCH --error logs/funprofiler_%a.error
 #SBATCH -n 1
 #SBATCH -N 1
 #SBATCH --qos=standard
