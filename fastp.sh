c825bc26e02032389566673170bf2aa78c10be14
AlvaroRodriguezDelRio <40565547+AlvaroRodriguezDelRio@users.noreply.github.com>
2025-05-12 12:36:27 +0200

    Update fastp.sh

--- fastp.sh
+++ fastp.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --array=1-X # Number of metagenomic samples
-#SBATCH --output logs/%A_%a.out
-#SBATCH --error logs/%A_%a.error
+#SBATCH --output logs/fastp_%a.out
+#SBATCH --error logs/fastp_%a.error
 #SBATCH -n 1
 #SBATCH -N 1
 #SBATCH --qos=standard
