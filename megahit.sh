b9bb96ab1b28abf68640261a8c83201972c6990b
AlvaroRodriguezDelRio <40565547+AlvaroRodriguezDelRio@users.noreply.github.com>
2025-09-11 10:57:46 +0200

    Update megahit.sh

--- megahit.sh
+++ megahit.sh
@@ -9,9 +9,10 @@
 #SBATCH --mem=150G
 
 ###Paths to files
-file1=$(cat paths_concat.txt | awk -v line=$SLURM_ARRAY_TASK_ID '{if (NR == line) print $0}')
-name=$(basename $file1 | sed 's/.fastq//g')
-file2=$(echo $file1 | sed 's/_1.fastq/_2.fastq/g')
+file1=$(cat  paths_clean_1.txt | awk -v line=$SLURM_ARRAY_TASK_ID '{if (NR == line) print $0}')
+name=$(basename $file1 | sed 's/_kneaddata_paired_1.fastq//g')
+file2=$(echo $file1 | sed 's/_kneaddata_paired_1.fastq/_kneaddata_paired_2.fastq/g')
+
 
 rm -rf out_megahit/$name
 /scratch/alvarordr/soft/MEGAHIT-1.2.9-Linux-x86_64-static/bin/megahit -1 $file1 -2 $file2 -o out_megahit/$name -m 0.90 --mem-flag 1 -t 30
