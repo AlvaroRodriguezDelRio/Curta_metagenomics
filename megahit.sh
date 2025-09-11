e959b3526f195aaa67a912935afbd0b7d393b584
AlvaroRodriguezDelRio <40565547+AlvaroRodriguezDelRio@users.noreply.github.com>
2025-09-11 10:55:47 +0200

    Create megahit.sh

---
+++ megahit.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --array=1-NSAMPLES
+#SBATCH --output logs/%A_%a.out
+#SBATCH --error logs/%A_%a.error
+#SBATCH -n 30
+#SBATCH -N 1
+#SBATCH --qos=standard
+#SBATCH --time=14-00:00
+#SBATCH --mem=150G
+
+###Paths to files
+file1=$(cat paths_concat.txt | awk -v line=$SLURM_ARRAY_TASK_ID '{if (NR == line) print $0}')
+name=$(basename $file1 | sed 's/.fastq//g')
+file2=$(echo $file1 | sed 's/_1.fastq/_2.fastq/g')
+
+rm -rf out_megahit/$name
+/scratch/alvarordr/soft/MEGAHIT-1.2.9-Linux-x86_64-static/bin/megahit -1 $file1 -2 $file2 -o out_megahit/$name -m 0.90 --mem-flag 1 -t 30
