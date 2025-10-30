e3c6eac55485a7c29a132cb52d0c310df5e75841
AlvaroRodriguezDelRio <40565547+AlvaroRodriguezDelRio@users.noreply.github.com>
2025-10-30 14:09:01 +0100

    Update prodigal.sh

--- prodigal.sh
+++ prodigal.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-#SBATCH --array=1-#N_SAMPLES
+#SBATCH --array=1-#N_CHUNKS
 #SBATCH --output logs/%A_%a.out
 #SBATCH --error logs/%A_%a.error
 #SBATCH -n 1
 #SBATCH -N 1
 #SBATCH --qos=standard
-#SBATCH --time=14-00:00
+#SBATCH --time=5-00:00
 #SBATCH --mem=2G
 
 export PATH=/scratch/alvarordr/soft/miniconda3/bin/:$PATH
@@ -13,6 +13,6 @@ export PATH=/scratch/alvarordr/soft/miniconda3/bin/:$PATH
 mkdir gff proteins cds
 
 ###Paths to files
-file1=$(cat  ../assembly/megahit/paths_chunks.txt | awk -v line=$SLURM_ARRAY_TASK_ID '{if (NR == line) print $0}')
+file1=$(cat  paths_chunks.txt | awk -v line=$SLURM_ARRAY_TASK_ID '{if (NR == line) print $0}')
 name=$(basename $file1)
 prodigal -i $file1 -o gff/$name.gff -a proteins/$name.faa -d cds/$name.fna -p meta -f gff
